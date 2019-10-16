781259c40d532f008663d18f3f9df7ebe62d34f1
browniu <niuniusoga@qq.com>
2019-10-17 05:31:38 +0800

    完成基本逻辑展示

--- src/draw.js
+++ src/draw.js
@@ -1,72 +1,78 @@
-// 暴露全局变量
-var cvs, ctx;
-var p1, p2, p3;
-
 // 初始化画布
 const initCanvas = (root) => {
-    cvs = document.createElement('canvas');
+    const cvs = document.createElement('canvas');
     cvs.height = 200;
     cvs.width = 200;
     cvs.style.background = '#f1f1f1';
     root.appendChild(cvs);
-    ctx = cvs.getContext('2d')
+    const ctx = cvs.getContext('2d');
+    return {cvs, ctx}
 };
 
-// 绘制矩形
-const drawPoint = (x, y, color, correction) => {
-    clearCvs();
-    if (!correction) {
-        x = x - 5;
-        y = y - 5;
-    }
-    ctx.fillStyle = color || 'red';
-    ctx.fillRect(x, y, 10, 10);
+// 绘制点位
+function drawPoints(points, cvs, ctx, color, size) {
+    clearCtx(cvs, ctx);
+    points.forEach(point => {
+        drawPoint(ctx, point[0], point[1], color, size)
+    })
+}
+
+// 绘制点
+const drawPoint = (ctx, x, y, color, size) => {
+    ctx.fillStyle = color || 'green';
+    ctx.beginPath();
+    ctx.arc(x, y, size || 5, 0, 2 * Math.PI);
     ctx.fill();
-    return {x1: x, x2: x + 10, y1: y, y2: y + 10}
 };
 
-// 监听拖拽
-function listenCvs(cvs) {
+// 交互监听
+function listenCvs(cvs, ctx, points, stempCode) {
+    let dragAble = [false, false, false];
     cvs.addEventListener('touchstart', (e) => {
         const touch = getPosRelaParent(e.touches[0], cvs);
-        if (touch && touchPoint(touch, p1)) {
-            feelTouch(touch)
-        }
+        dragAble = touchPoints(touch, points)
     });
+
     cvs.addEventListener('touchmove', (e) => {
         const touch = getPosRelaParent(e.changedTouches[0], cvs);
-        if (touch && p1.enable) {
-            drawPoint(touch.x, touch.y, 'green')
-            displayPointPos(touch, document.querySelector('#pps1'))
+        if (touch && dragAble.includes(true)) {
+            points = points.map((point, i) => {
+                if (dragAble[i]) return [touch.x, touch.y];
+                else return point
+            });
+            const scope = displayPoints(points, document.querySelector('.panel'), stempCode)
+            drawPoints(points, cvs, ctx, scope > 92 ? 'green' : 'red');
         }
     });
-    cvs.addEventListener('touchend', (e) => {
-        const touch = getPosRelaParent(e.changedTouches[0], cvs);
-        if (touch && p1.enable) {
-            p1 = drawPoint(touch.x, touch.y)
-        }
 
+    cvs.addEventListener('touchend', () => dragAble = [false, false, false]);
+
+    // document.querySelector('.rotate').onclick = () => rotateCtx(cvs, ctx, points);
+
+}
+
+// 判断接触状态
+function touchPoints(touch, points) {
+    return points.map(point => {
+        const xIn = touch.x >= point[0] - 10 && touch.x <= point[0] + 10;
+        const yIn = touch.y >= point[1] - 10 && touch.y <= point[1] + 10;
+        return xIn && yIn
     })
-
 }
 
-// 显示点的坐标
-function displayPointPos(point, root) {
-    const xDom = root.firstChild;
-    const yDom = root.lastChild;
-    xDom.innerText = point.x;
-    yDom.innerText = point.y;
-}
-
-// 感知点触摸
-function feelTouch(touch) {
-    clearCvs();
-    p1 = drawPoint(touch.x, touch.y, 'green');
-    p1.enable = true
+// 显示点组坐标
+function displayPoints(points, root, stempCode) {
+    const code = distanceMultiple(points);
+    const scope = approximateMatchArray(code, stempCode);
+    const status = document.querySelector('.status').firstChild;
+    status.style.width = scope + '%';
+    status.style.background = scope < 92 ? 'red' : 'green';
+    root.innerHTML = `<p style="font-size: 12px;opacity: .7;">${scope}</p>`; // + points.map(point => `<p>${JSON.stringify(point)}</p>`).join('')
+    return scope
 }
 
 // 清空画布
-function clearCvs() {
+function clearCtx(cvs, ctx) {
     ctx.clearRect(0, 0, cvs.height, cvs.width);
 }
 
@@ -81,23 +87,27 @@ function getPosRelaParent(event, parent) {
 
 }
 
-// 检测是否接触到某个点位
-function touchPoint(touch, point) {
-    const xIn = touch.x >= point.x1 && touch.x <= point.x2;
-    const yIn = touch.y >= point.y1 && touch.y <= point.y2;
-    return xIn && yIn
+// 绘制固定图章
+function drawStamp(root, points) {
+    const {cvs, ctx} = initCanvas(root);
+    cvs.style.margin = '0 10px';
+    drawPoints(points, cvs, ctx, 'green')
 }
 
-// 初始化面板
-function initPanel() {
-    const panel = document.createElement()
+// 旋转画布
+function rotateCtx(cvs, ctx, points) {
+    clearCtx(cvs, ctx);
+    ctx.rotate(20 * Math.PI / 180);
+    drawPoints(points, cvs, ctx);
 }
 
 // 绘制运行
-const drawRun = () => {
-    initCanvas(document.querySelector('.cvs'));
-    p1 = drawPoint(10, 10);
-    listenCvs(cvs)
-};
-window.onload = () => drawRun();
+function draw(points) {
+    const {cvs, ctx} = initCanvas(document.querySelector('.cvs'));
+    points = points || [[126, 77], [79, 133], [61, 43]];
+    const stempCode = distanceMultiple(points);
+    drawPoints(points, cvs, ctx);
+    listenCvs(cvs, ctx, points, stempCode);
+    drawStamp(document.querySelector('.cvs'), points)
+}
 
